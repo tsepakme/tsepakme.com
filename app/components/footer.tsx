41ba5465a8f20a44f80641298e666c98ccf15da5
Aiusha Mikhailov <a.mikhaylov@hirschtec.eu>
2024-09-17 14:10:35 +0400

    added subject mailto link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -68,7 +68,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="mailto:ayushamikhaylov@gmail.com"
+            href="mailto:ayushamikhaylov@gmail.com?subject=Mail from Website"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">mail</p>
